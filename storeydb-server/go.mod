5ebe5e2b893d729f63422eb1b0d4195e37a81bf4
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-09-25 17:44:37 -0400

    Add backend and frontend stencils

---
+++ storeydb-server/go.mod
@@ -0,0 +1,8 @@
+module storeydb-server/v0
+
+go 1.16
+
+require (
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
---
+++ storeydb-server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/stories", StoriesHandler)
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	PORT := "5000"
+	fmt.Println("Listening on port " + PORT)
+
+	corsHandler := handlers.CORS(originsOk, headersOk, methodsOk)(r)
+
+	http.ListenAndServe(":"+PORT, corsHandler)
+	// http.Handle("/", r)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+}
+
+func StoriesHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+}
