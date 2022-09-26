faa05830bff23bd43e51ad24fd9c612727531c59
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-09-25 21:26:31 -0400

    Add app header

--- storeydb-site/src/App.js
+++ storeydb-site/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 
 import SideCarousel from './components/SideCarousel';
+import AppHeader from './components/AppHeader';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader/>
       <header className="App-header">
-
-        storeydb
-
         <SideCarousel/>
       </header>
     </div>
