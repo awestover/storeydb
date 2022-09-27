c9659c84e1e2527a77e853f8c28411454029fe68
awestover <alek.westover@gmail.com>
2022-09-26 22:13:04 -0400

    got a little popup dude added

--- site/src/App.js
+++ site/src/App.js
@@ -2,16 +2,9 @@ import './App.css';
 
 import SideCarousel from './components/SideCarousel';
 import AppHeader from './components/AppHeader';
+import MyPopup from './components/Popup';
 
 function App() {
-
-
-  // lol i couldnt figure out jquery so i do this
-  // let xhr = new XMLHttpRequest();
-  // xhr.open("GET", "http://localhost:5000/bigdata");
-  // xhr.send();
-  // xhr.onload = () => {console.log(xhr.responseText); alert(xhr.responseText)} ;
-
   return (
     <div className="App">
       <AppHeader/>
@@ -20,6 +13,8 @@ function App() {
         <SideCarousel contentType="location"/>
         <SideCarousel contentType="item"/>
       </header>
+
+      <MyPopup/>
     </div>
   );
 }
