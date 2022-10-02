9f183c2125d5eaca73f7177c25dc132d9ee22463
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-10-01 23:18:24 -0400

    Read element and story data from json

--- server/main.go
+++ server/main.go
@@ -47,30 +47,17 @@ type ElementData struct {
 func GetElement(w http.ResponseWriter, r *http.Request) {
 	eltType := r.URL.Query().Get("name")
 
-	elt := [4]ElementData{}
+	elts := map[string][]ElementData{}
+	filename := "db/elts.json"
+	jsonFile, _ := os.Open(filename)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &elts)
 
-	// todo: read this from a db
-	if eltType == "character" {
-		elt[0] = ElementData{Name: "Bob", Description: "he is a really stsrong dude"}
-		elt[1] = ElementData{Name: "Kevin", Description: "he is a wizard. magical powers include insane coding skillz"}
-		elt[2] = ElementData{Name: "jumpydude", Description: "he is a nice guy. "}
-		elt[3] = ElementData{Name: "theland dog", Description: "he eats grass"}
-		jsonData, _ := json.Marshal(elt)
-		io.WriteString(w, string(jsonData))
-	} else if eltType == "location" {
-		elt := [2]ElementData{}
-		elt[0] = ElementData{Name: "narnia", Description: "this is a nice place"}
-		elt[1] = ElementData{Name: "skadrial", Description: "so cold"}
-		jsonData, _ := json.Marshal(elt)
-		io.WriteString(w, string(jsonData))
-	} else {
-		elt := [2]ElementData{}
-		elt[0] = ElementData{Name: "rip", Description: "asddffasjdlkffasdff"}
-		elt[1] = ElementData{Name: "asdff", Description: "asdff"}
-		jsonData, _ := json.Marshal(elt)
-		io.WriteString(w, string(jsonData))
-	}
+	jsonData, _ := json.Marshal(elts[eltType])
+	io.WriteString(w, string(jsonData))
 
+	fmt.Println(jsonData)
+	fmt.Println(elts)
 	fmt.Println("yoyo yo big data")
 	fmt.Println(eltType)
 }
@@ -88,21 +75,11 @@ type Stories struct {
 func getStories(w http.ResponseWriter, r *http.Request) {
 	whichStory := r.URL.Query().Get("storyid")
 
-	// byteValue, _ := ioutil.ReadAll("db/elts.json")
-	jsonFile, _ := os.Open("db/story.json")
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
 	stories := map[string]Stories{}
+	filename := "db/story.json"
+	jsonFile, _ := os.Open(filename)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 	json.Unmarshal(byteValue, &stories)
-
-	// s1 := StoryData{
-	//   Title:   "intro",
-	//   PoV:     "kevin",
-	//   Summary: "kevin makes a really cool webapp",
-	//   Scene:   " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "}
-
-	// s2 := StoryData{Title: "funnytitle", PoV: "alek", Summary: "alek does his math homework", Scene: "once upon a time there was a priority queue in the RAM model that supported insert decrease key and delete min opperations in log log u time where u is the set of possible keys"}
 
 	jsondata, _ := json.Marshal(stories[whichStory])
 	io.WriteString(w, string(jsondata))
