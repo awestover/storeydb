d55b4944217513ab00c2f3518b9624c95d28baea
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-10-29 15:43:35 -0400

    Change image cards for view vs edit

--- site/src/components/ImageCards.js
+++ site/src/components/ImageCards.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
+import { Card, Box } from '@mui/material';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
@@ -15,32 +15,40 @@ import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-import {EditPopup} from './Popup.js'
+import { EditPopup, ViewPopup } from './Popup.js'
 
 export default function ImageCards(props) {
   const [expanded, setExpanded] = React.useState(false);
 
   return (
-    <Card sx={{ width: 250, height: 300 }}>
+    <Card sx={{ width: 250, backgroundColor: "transparent" }}>
 
-      <CardMedia
-        component="img"
-        height="150"
-        image="https://images.unsplash.com/photo-1664055258388-6efc6fbe0aa3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-        alt="Paella dish"
-      />
 
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          { props.name }
-        </Typography>
+        <CardMedia
+          component="img"
+          height="150"
+          image="https://images.unsplash.com/photo-1664055258388-6efc6fbe0aa3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          alt="Paella dish"
+        />
 
-        <Typography variant="body2" color="text.primary">
-          { props.description }
-        </Typography>
+          <Typography variant="h5" color="white" m={1}>
+            { props.name }
+          </Typography>
+        <Box display="flex" justifyContent="center" gap={2}>
+          <ViewPopup updateData={props.updateData} curName={props.name} curDescription={props.description}/>
+          <EditPopup updateData={props.updateData} curName={props.name} curDescription={props.description}/>
+        </Box>
+
+        { 
+          // <Typography variant="body2" color="text.primary">
+          //   { props.description }
+          // </Typography>
+        }
+
+
       </CardContent>
 
-    <EditPopup updateData={props.updateData} curName={props.name} curDescription={props.description}/>
 
     </Card>
   );
